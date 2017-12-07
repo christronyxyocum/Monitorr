5f2073c32a5c514ce9014f03a0364c3881b5ac48
jonfinley <accounts@jonathanfinley.net>
2017-12-06 23:30:57 -0500

    formatting.

--- settings.php
+++ settings.php
@@ -43,10 +43,9 @@ by @seanvree, @wjbeckett, and @jonfinley
 						text-align: center;
 				}
 		</style>
-
+		<?php include ('assets/config.php'); ?>
 		<?php include ('assets/php/check.php') ;?>
 		<?php include ('assets/php/gitinfo.php'); ?>
-		<?php include ('assets/config.php'); ?>
 
 		<script src="assets/js/jquery.min.js"></script>
 
